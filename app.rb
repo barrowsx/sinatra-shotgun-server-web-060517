43bc0a9c9f9c4ab81dfc99a42453648e31cec0ad
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-06-26 14:16:52 -0400

    Committed using gotgit (Thanks Daniel's friend!)

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "https://twitter.com/NintendoAmerica/status/879369032947847168"
   end
 
 end
